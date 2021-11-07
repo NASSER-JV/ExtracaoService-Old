367217559dbde9002bc0b3debfef32edb7aa88f2
João Vitor Goncalves Pina <joaovitorpinag@hotmail.com>
2021-11-07 20:17:10 -0300

    Adicionado arquivo de exemplo das variaveis de ambiente e configuracao para carregar o arquivo
    Signed-off-by: João Vitor Goncalves Pina <joaovitorpinag@hotmail.com>

--- ExtracaoService/Program.cs
+++ ExtracaoService/Program.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using ExtracaoService.Data;
+using Microsoft.Extensions.Configuration;
 
 namespace ExtracaoService
 {
     class Program
     {
+        public static IConfigurationRoot Config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+
         static void Main(string[] args)
         {
             var empresas = Environment.GetEnvironmentVariable("Companies");
